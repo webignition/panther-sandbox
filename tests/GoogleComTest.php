be9c2745de70bf1fe9b02960141b837168648e2e
Jon Cram <webignition@gmail.com>
2019-04-30 11:22:39 +0100

    Implement basic google.com test (#7)

---
+++ tests/GoogleComTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace webignition\PantherSandbox\Tests;
+
+use Facebook\WebDriver\Remote\RemoteWebElement;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Panther\Client;
+
+class GoogleComTest extends TestCase
+{
+    public function testQuery()
+    {
+        $client = Client::createChromeClient();
+        $crawler = $client->request('GET', 'https://www.google.com');
+
+        $this->assertEquals('Google', $client->getTitle());
+
+        /* @var RemoteWebElement $input */
+        $input = $crawler->filter('.gLFyf.gsfi')->getElement(0);
+        $this->assertInstanceOf(RemoteWebElement::class, $input);
+
+        /* @var RemoteWebElement $searchButton */
+        $searchButton = $crawler->filter('.FPdoLc.VlcLAe input[name=btnK]')->getElement(0);
+        $this->assertInstanceOf(RemoteWebElement::class, $searchButton);
+
+        if ($input instanceof RemoteWebElement) {
+            $input->sendKeys('example');
+        }
+
+        if ($searchButton instanceof RemoteWebElement) {
+            $searchButton->submit();
+        }
+
+        $this->assertEquals('example - Google Search', $client->getTitle());
+    }
+}
